94fe1149d657adc896c124bf7add89a15b0cd7f9
Francesco Stablum <stablum@gmail.com>
2016-06-27 20:22:44 +0200

    interesting...

--- theano_decoder.py
+++ theano_decoder.py
@@ -6,16 +6,17 @@ import pandas as pd
 from tqdm import trange,tqdm
 import numpy as np
 from sklearn.preprocessing import normalize
+import sklearn.svm
+
 theano.config.exception_verbosity="high"
-#theano.config.optimizer='fast_run'
-train_filename = 'adult/adult.data'
-test_filename = 'adult/adult.test'
-lr_begin = 10.
+theano.config.optimizer='None'
+train_filename = 'ionosphere/ionosphere.data'
+lr_begin = 0.5
 lr_annealing_T=1
 lr=None
 n_epochs = 10000
 sigma_x = 1.
-sigma_z = 1e6
+sigma_z = 1e4
 nonlinearity = T.nnet.sigmoid
 
 def calculate_lr(t):
@@ -32,24 +33,24 @@ def load_file(filename):
     df = df.fillna("not_available")
     return df
 
-def shuffle(Z,X):
+def shuffle(Z,X,Y):
     sel = np.arange(X.shape[0])
     np.random.shuffle(sel)
     X = X[sel,:]
     Z = Z[sel,:]
-    return Z,X
+    Y = Y[sel,:]
+    return Z,X,Y
 
 def load_data():
-    df_train = load_file(train_filename)
-    df_test = load_file(test_filename)
-    df = pd.concat([df_train,df_test])
-    ddf = pd.get_dummies(df)
-    XY = ddf.as_matrix()
-    X = XY[:,:-2]
-    Y = XY[:,[-2]]
-    X = normalize(X,axis=0)
-    X,Y = shuffle(X,Y)
-    cutpoint = int(X.shape[0]*0.75)
+    df = load_file(train_filename)
+    #ddf = pd.get_dummies(df)
+    XY = df.as_matrix()
+    X = XY[:,:-1].astype('float')
+    Y = np.expand_dims((df.iloc[:,-1] == 'g').astype('float').as_matrix(),1)
+    #Y = XY[:,[-1]]
+    #X = normalize(X,axis=0)
+    X,Y,_ = shuffle(X,Y,Y)
+    cutpoint = int(X.shape[0]*0.95)
     X_train = X[:cutpoint,:]
     Y_train = Y[:cutpoint,:]
     X_test = X[cutpoint:,:]
@@ -66,8 +67,12 @@ def build_net():
     lin2 = T.dot(W2, out1)
     lin2.name = 'lin2'
     out2 = nonlinearity(lin2)
+    W3 = T.dmatrix('W3')
+    lin3 = T.dot(W3, out2)
+    lin3.name = 'lin3'
+    out3 = nonlinearity(lin3)
 
-    return inputs, [W1,W2], out2
+    return inputs, [W1,W2,W3], out3
 
 def update(learnable, grad):
     learnable -= lr * grad
@@ -82,8 +87,8 @@ def step(z, x, Ws_vals, grad_fn):
     #    print(z_grads)
     update(z,z_grads)
 
-def train(Z, X, Ws_vals, grad_fn):
-    for z,x in tqdm(list(zip(Z,X))):
+def train(Z, X, Ws_vals, grad_fn,repeat=1):
+    for z,x in tqdm(list(zip(Z,X))*repeat):
         step(z, x, Ws_vals, grad_fn)
 
 def nll_sum(Z, X, Ws_vals, nll_fn):
@@ -96,8 +101,15 @@ def nll_sum(Z, X, Ws_vals, nll_fn):
 def build_negative_log_likelihood(z,outputs,x):
     error_term = 1/sigma_x * T.sum((x-outputs)**2)
     prior_term = 1/sigma_z * T.sum((z)**2)
-    nll = error_term #+ prior_term
+    nll = error_term + prior_term
     return nll
+
+def test_classifier(Z,Y):
+    #classifier = sklearn.svm.SVC()
+    classifier = sklearn.svm.SVC(kernel='linear')
+    classifier.fit(Z,Y[:,0])
+    svc_score = classifier.score(Z,Y[:,0])
+    print("SVC score: %s"%svc_score)
 
 def main():
     np.set_printoptions(precision=4, suppress=True)
@@ -105,10 +117,12 @@ def main():
     x_dim = X.shape[1]
     latent_dim=2
     Z = (np.random.random((X.shape[0],latent_dim))-0.5) * 1
-    H1=11 # dimensionality of hidden layer
+    H1=5# dimensionality of hidden layer
+    H2=17# dimensionality of hidden layer
     W1_vals = np.random.random((H1,latent_dim))*0.1
-    W2_vals = np.random.random((x_dim,H1))*0.1
-    Ws_vals = [W1_vals,W2_vals]
+    W2_vals = np.random.random((H2,H1))*0.1
+    W3_vals = np.random.random((x_dim,H2))*0.1
+    Ws_vals = [W1_vals,W2_vals,W3_vals]
     # set up
     z, Ws, outputs = build_net()
     x = T.dvector('x')
@@ -134,9 +148,10 @@ def main():
     for epoch in range(n_epochs):
         global lr
         lr = calculate_lr(epoch)
-        Z,X = shuffle(Z,X)
+        Z,X,Y = shuffle(Z,X,Y)
         summary()
-        train(Z,X,Ws_vals,grad_fn)
+        test_classifier(Z,Y)
+        train(Z,X,Ws_vals,grad_fn,repeat=1)
         np.save("theano_decoder_Z.npy",Z)
     summary()
 if __name__=="__main__":
