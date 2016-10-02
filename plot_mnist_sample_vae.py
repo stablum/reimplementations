7c0ae40f647cc02bc1ddb84d106552bd31f683b1
Francesco Stablum <stablum@gmail.com>
2016-10-02 14:52:56 +0200

    plot samples from vae

---
+++ plot_mnist_sample_vae.py
@@ -0,0 +1,26 @@
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+
+def main():
+    rasters = []
+    for filename in sys.argv[1:]:
+        a = np.load(filename)
+        print("a.shape",a.shape)
+        aa = a[0,0,:10,:]
+        print("aa.shape",aa.shape)
+        for curr in aa:
+            print("curr.shape",curr.shape)
+            try:
+                raster = np.reshape(curr,(28,28))
+            except Exception as e:
+                print ("curr.shape",curr.shape)
+                raise e
+            raster = np.reshape(curr,(28,28))
+            rasters.append(raster)
+    rasters_stacked = np.hstack(rasters)
+    plt.imshow(rasters_stacked)
+    plt.show()
+
+if __name__=="__main__":
+    main()
