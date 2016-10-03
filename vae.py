080210ec508f11fa8232e186624c7cc52c7b6a0d
Francesco Stablum <stablum@gmail.com>
2016-10-03 19:18:31 +0200

    whoops

--- vae.py
+++ vae.py
@@ -230,8 +230,7 @@ def build_obj(z_sample,z_mu,z_sigma,x_orig,x_out):
         z_sigma_inv,#11
         z_sigma_fixed,#12
         C,#13
-        reconstruction_error_const,#14
-        reconstruction_error_proper,#15
+        reconstruction_error_proper,#14
     ]
 
 def test_classifier(Z,Y):
